1b88bb72b21a4ebd8e4da459faa64e5f9b3444bb
unknown <admin@VLAN-25-58-310.rd.mirea.ru>
2018-04-13 20:48:37 +0300

    UpdateserverAndAddFileA

---
+++ server/server.js
@@ -0,0 +1,9 @@
+﻿var http = require("http");
+var fs = require("fs"); 
+http.createServer(function(request, response){
+     
+    response.setHeader("Content-Type", "text/html");
+	var body = fs.readFileSync("FileA.html");
+    response.write(body);
+    response.end();
+}).listen(80);
